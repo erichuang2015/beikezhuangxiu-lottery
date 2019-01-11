ed4b1db74c147dd59181fee97eda64cd9629955d
Zhu Wei <1204953543@qq.com>
2019-01-11 21:23:44 +0800

    add:换背景图+将中奖者存储于localstorage+重置清空中奖信息

--- src/App.js
+++ src/App.js
@@ -5,93 +5,116 @@ import { Select, Button, Icon, message } from 'antd'
 import moment from 'moment'
 const { Option } = Select
 
-//奖项reward暂时不使用，如果要用，将render里的Select注释去掉即可设置奖项
+// 奖项reward暂时不使用，如果要用，将render里的Select注释去掉即可设置奖项
 
 class App extends Component {
   state={
-    reward:'',
-    person:0,
-    rewardArray:[],
-    showList:false,
-    showLucky:false,
-    showButton:true
+    // reward: '',
+    person: 0,
+    tmpArray: [],
+    showList: false,
+    showLucky: false,
+    showButton: true
   }
-  
+
+  componentWillMount () {
+    // 再次刷新页面时，删除中奖者
+    if (localStorage.getItem('rewardArray')) {
+      let array = JSON.parse(localStorage.getItem('rewardArray'))
+      array.map((value) => {
+        value.person.map((val) => {
+          let indexInMember = member.findIndex((v) => {
+            return val.name === v.name
+          })
+          if (indexInMember >= 0) {
+            member.splice(indexInMember, 1)
+          }
+        })
+      })
+    }
+  }
+
   componentDidMount = () => {
     try {
-      window.TagCanvas.Start('myCanvas','',{
-        imageRadius:50,
-        imageScale:1.3,
+      window.TagCanvas.Start('myCanvas', '', {
+        imageRadius: 50,
+        imageScale: 1.3,
         dragControl: 1,
-        noMouse:true,
+        noMouse: true,
         textHeight: 14,
-        initial:[0.2,-0.1],
+        initial: [0.2, -0.1],
         reverse: true,
         depth: 0.8,
         maxSpeed: 0.05
       })
-    } catch(e) {
+    } catch (e) {
       // something went wrong, hide the canvas container
       // document.getElementById('myCanvasContainer').style.display = 'none';
-      console.log('error',e)
+      console.log('error', e)
     }
   }
 
-  handleRewardChange = (value) => {
-    console.log(`selected ${value}`)
-    if(value === '其他')value=''
-    this.setState({
-      reward:value
-    })
-  }
+  // handleRewardChange = (value) => {
+  //   console.log(`selected ${value}`)
+  //   if (value === '其他')value = ''
+  //   this.setState({
+  //     reward: value
+  //   })
+  // }
 
   handlePersonChange = (value) => {
-    console.log(`selected ${value}`)
     this.setState({
-      person:value
+      person: value
     })
   }
 
-  //加快转速
+  // 加快转速
   start = () => {
-    //如果抽奖人数大于存在人数，不允许抽奖
-    if(member.length < this.state.person){
+    // 如果抽奖人数大于存在人数，不允许抽奖
+    if (member.length < this.state.person) {
       message.warning('人数不够再抽奖啦！')
       this.setState({
-        showButton:true
-        })
+        showButton: true
+      })
       window.TagCanvas.SetSpeed('myCanvas', [0.2, -0.1])
       return
     }
-    //未选择抽奖人数，不允许抽奖
-    if(this.state.person === 0){
+    // 未选择抽奖人数，不允许抽奖
+    if (this.state.person === 0) {
       message.warning('请选择抽奖人数！')
       return
     }
     this.setState({
-      showButton:false
+      showButton: false
     })
     let speed = Math.random()
-    window.TagCanvas.SetSpeed('myCanvas', [speed+2,speed])
-    window.interval = setInterval(()=>{
+    window.TagCanvas.SetSpeed('myCanvas', [speed + 2, speed])
+    window.interval = setInterval(() => {
       speed = Math.random()
-      window.TagCanvas.SetSpeed('myCanvas', [speed+2,speed])
-    },1000)
+      window.TagCanvas.SetSpeed('myCanvas', [speed + 2, speed])
+    }, 1000)
   }
 
-  //使转速正常，抽取中奖人员放入数组，显示本轮中奖名单
+  // 使转速正常，抽取中奖人员放入数组，显示本轮中奖名单
   stop = () => {
-    //公平抽奖的逻辑方法lottery：从member.length人中随机抽person人
-    let luckyDog = this.lottery(member, this.state.person) //返回中奖人员数组
+    // 公平抽奖的逻辑方法lottery：从member.length人中随机抽person人
+    let luckyDog = this.lottery(member, this.state.person) // 返回中奖人员数组
     let thisTurnToShow = {
-      reward: this.state.reward || this.state.rewardArray.length+1,
+      reward: localStorage.getItem('rewardArray') ? JSON.parse(localStorage.getItem('rewardArray')).length + 1 : 1,
       time: moment().format('lll'),
-      person:luckyDog
+      person: luckyDog
     }
-    this.state.rewardArray.push(thisTurnToShow)
+    this.state.tmpArray.push(thisTurnToShow)
+    if (localStorage.getItem('rewardArray')) {
+      let array = JSON.parse(localStorage.getItem('rewardArray'))
+      array.push(thisTurnToShow)
+      localStorage.setItem('rewardArray', JSON.stringify(array))
+    } else {
+      localStorage.setItem('rewardArray', JSON.stringify(this.state.tmpArray))
+    }
     this.setState({
-      showButton:true,
-      showLucky:true
+      showButton: true,
+      showLucky: true
     })
     clearInterval(window.interval)
     window.TagCanvas.SetSpeed('myCanvas', [0.2, -0.1])
@@ -100,83 +123,88 @@ class App extends Component {
   closeLucky = () => {
     window.TagCanvas.Reload('myCanvas')
     this.setState({
-      showLucky:false
+      showLucky: false
     })
   }
 
-  //显示所有中奖名单
+  // 显示所有中奖名单
   showList = () => {
-    if(this.state.rewardArray.length === 0){
+    if (!localStorage.getItem('rewardArray')) {
       message.warning('请先点击开始进行抽奖！')
       return
     }
     this.setState({
-      showList:true
+      showList: true
     })
   }
 
-  //从一个数组里随机选n个人，返回人员数组，并把那n个人从原数组中去除
+  // 从一个数组里随机选n个人，返回人员数组，并把那n个人从原数组中去除
   lottery = (array, n) => {
     let returnArray = []
-    while(n > 0){
-      let randomNum = Math.floor(Math.random()*array.length)
+    while (n > 0) {
+      let randomNum = Math.floor(Math.random() * array.length)
       returnArray.push(array[randomNum])
-      array.splice(randomNum,1)
+      array.splice(randomNum, 1)
       n = n - 1
     }
     return returnArray
   }
 
+  clearLocalStorage=() => {
+    localStorage.removeItem('rewardArray')
+    window.location.reload()
+  }
+
   render () {
     return (
       <div className='App'>
-        <div className="header">
-          <img src={require('./img/beike.jpg')} className='head-img' alt=""/>
+        <div className='header'>
+          <img src={require('./img/beike.jpg')} className='head-img' alt='' />
           <h1 className='head-title'>贝壳装修抽奖系统</h1>
         </div>
-        <div id="myCanvasContainer">
-          <canvas width="800" height="800" id="myCanvas">
+        <div id='myCanvasContainer'>
+          <canvas width='800' height='800' id='myCanvas'>
             <p>Anything in here will be replaced on browsers that support the canvas element</p>
             <ul>
-              {member.map((val,idx)=>{
-                  return (
-                    <li key={idx}><a href="/no-use-href"><img src={require(`${val.img}`)} alt="" width="50" height="50"/></a></li>
-                  )
-                })}
+              {member.map((val, idx) => {
+                return (
+                  <li key={idx}><a href='/no-use-href'><img src={require(`${val.img}`)} alt='' width='50' height='50' /></a></li>
+                )
+              })}
             </ul>
           </canvas>
         </div>
-        <div className="footer">
-          <div className="foot-left inline-block">
+        <div className='footer'>
+          <div className='foot-left inline-block'>
             {/* <Select defaultValue="奖项" className='select-style' onChange={this.handleRewardChange}>
               <Option value="一等奖">一等奖</Option>
               <Option value="二等奖">二等奖</Option>
               <Option value="三等奖">三等奖</Option>
               <Option value="其他">其他</Option>
             </Select> */}
-            <Select defaultValue="人数" className='select-style' onChange={this.handlePersonChange}>
-              <Option value="1">1人</Option>
-              <Option value="2">2人</Option>
-              <Option value="3">3人</Option>
-              <Option value="4">4人</Option>
-              <Option value="5">5人</Option>
+            <Select defaultValue='人数' className='select-style' onChange={this.handlePersonChange}>
+              <Option value='1'>1人</Option>
+              <Option value='2'>2人</Option>
+              <Option value='3'>3人</Option>
+              <Option value='4'>4人</Option>
+              <Option value='5'>5人</Option>
             </Select>
           </div>
           {this.state.showButton && <Button className='foot-mid inline-block' onClick={this.start} type='primary'>开始</Button>}
           {!this.state.showButton && <Button className='foot-mid inline-block' onClick={this.stop} type='primary'>停止</Button>}
           <Button className='foot-right inline-block' onClick={this.showList}>中奖名单</Button>
-        </div> 
+        </div>
         {/* 本轮中奖名单弹层 */}
         {this.state.showLucky && (
-          <div className="contain-wrap">
-            <Icon type='close' className="delete" onClick={this.closeLucky}></Icon>
-            <div className="wrap"></div>
-            <div className="lucky-title-word">中奖啦！！！</div>
-            <div className="lucky">
-              {this.state.rewardArray[this.state.rewardArray.length-1].person.map((val,idx)=>{
+          <div className='contain-wrap'>
+            <Icon type='close' className='delete' onClick={this.closeLucky} />
+            <div className='wrap' />
+            <div className='lucky-title-word'>中奖啦！！！</div>
+            <div className='lucky'>
+              {JSON.parse(localStorage.getItem('rewardArray'))[JSON.parse(localStorage.getItem('rewardArray')).length - 1].person.map((val, idx) => {
                 return (
                   <div key={idx} className='lucky-item'>
-                    <img src={require(`${val.img}`)} className='lucky-item-img' alt="" width="100" height="100"/>
+                    <img src={require(`${val.img}`)} className='lucky-item-img' alt='' width='100' height='100' />
                     <p className='lucky-word'>{val.name}</p>
                   </div>
                 )
@@ -186,19 +214,20 @@ class App extends Component {
         )}
         {/* 所有中奖人员名单弹层 */}
         {this.state.showList && (
-          <div className="contain-wrap">
-            <Icon type='close' className="delete" onClick={()=>this.setState({showList:false})}></Icon>
-            <div className="wrap"></div>
-            <div className="list">
-              {this.state.rewardArray.map((val,idx)=>{
+          <div className='contain-wrap'>
+            <Button className='reset' onClick={this.clearLocalStorage}>重置</Button>
+            <Icon type='close' className='delete' onClick={() => this.setState({ showList: false })} />
+            <div className='wrap' />
+            <div className='list'>
+              {localStorage.getItem('rewardArray') && JSON.parse(localStorage.getItem('rewardArray')).map((val, idx) => {
                 return (
-                  <div key={idx} className="list-item">
-                    <p className="list-title-word">{val.reward}、{val.time.toString()}</p>
-                    <div className="list-wrap">
-                      {val.person.map((value,index)=>{
+                  <div key={idx} className='list-item'>
+                    <p className='list-title-word'>{val.reward}、{val.time.toString()}</p>
+                    <div className='list-wrap'>
+                      {val.person.map((value, index) => {
                         return (
                           <div key={index} className='list-person-item'>
-                            <img src={require(`${value.img}`)} className='list-item-img' alt="" width="100" height="100"/>
+                            <img src={require(`${value.img}`)} className='list-item-img' alt='' width='100' height='100' />
                             <p className='list-person-word'>{value.name}</p>
                           </div>
                         )
--- src/member.js
+++ src/member.js
@@ -1,226 +1,226 @@
 export default [
   {
-    name:'张炎',
-    img:'./img/1.jpeg'
+    name: '张炎',
+    img: './img/1.jpeg'
   },
   {
-    name:'杨峰',
-    img:'./img/2.jpeg'
+    name: '杨峰',
+    img: './img/2.jpeg'
   },
   {
-    name:'唐小卓',
-    img:'./img/3.jpeg'
+    name: '唐小卓',
+    img: './img/3.jpeg'
   },
   {
-    name:'朱蔚',
-    img:'./img/4.jpeg'
+    name: '朱蔚',
+    img: './img/4.jpeg'
   },
   {
-    name:'李利德',
-    img:'./img/WechatIMG111.jpeg'
+    name: '李利德',
+    img: './img/WechatIMG111.jpeg'
   },
   {
-    name:'李磊',
-    img:'./img/WechatIMG59.jpeg'
+    name: '李磊',
+    img: './img/WechatIMG59.jpeg'
   },
   {
-    name:'李爽',
-    img:'./img/WechatIMG60.jpeg'
+    name: '李爽',
+    img: './img/WechatIMG60.jpeg'
   },
   {
-    name:'李添翼',
-    img:'./img/WechatIMG61.jpeg'
+    name: '李添翼',
+    img: './img/WechatIMG61.jpeg'
   },
   {
-    name:'刘源',
-    img:'./img/WechatIMG62.jpeg'
+    name: '刘源',
+    img: './img/WechatIMG62.jpeg'
   },
   {
-    name:'李星谣',
-    img:'./img/WechatIMG63.jpeg'
+    name: '李星谣',
+    img: './img/WechatIMG63.jpeg'
   },
   {
-    name:'李义文',
-    img:'./img/WechatIMG64.jpeg'
+    name: '李义文',
+    img: './img/WechatIMG64.jpeg'
   },
   {
-    name:'庞玉彤',
-    img:'./img/WechatIMG65.jpeg'
+    name: '庞玉彤',
+    img: './img/WechatIMG65.jpeg'
   },
   {
-    name:'彭世航',
-    img:'./img/WechatIMG66.jpeg'
+    name: '彭世航',
+    img: './img/WechatIMG66.jpeg'
   },
   {
-    name:'钱学敏',
-    img:'./img/WechatIMG67.jpeg'
+    name: '钱学敏',
+    img: './img/WechatIMG67.jpeg'
   },
   {
-    name:'史鹏飞',
-    img:'./img/WechatIMG68.jpeg'
+    name: '史鹏飞',
+    img: './img/WechatIMG68.jpeg'
   },
   {
-    name:'唐莉',
-    img:'./img/WechatIMG69.jpeg'
+    name: '唐莉',
+    img: './img/WechatIMG69.jpeg'
   },
   {
-    name:'王春明',
-    img:'./img/WechatIMG70.jpeg'
+    name: '王春明',
+    img: './img/WechatIMG70.jpeg'
   },
   {
-    name:'夏文海',
-    img:'./img/WechatIMG71.jpeg'
+    name: '夏文海',
+    img: './img/WechatIMG71.jpeg'
   },
   {
-    name:'徐辛承',
-    img:'./img/WechatIMG72.jpeg'
+    name: '徐辛承',
+    img: './img/WechatIMG72.jpeg'
   },
   {
-    name:'岳奎',
-    img:'./img/WechatIMG73.jpeg'
+    name: '岳奎',
+    img: './img/WechatIMG73.jpeg'
   },
   {
-    name:'岳贤强',
-    img:'./img/WechatIMG74.jpeg'
+    name: '岳贤强',
+    img: './img/WechatIMG74.jpeg'
   },
   {
-    name:'张浩宇',
-    img:'./img/WechatIMG75.jpeg'
+    name: '张浩宇',
+    img: './img/WechatIMG75.jpeg'
   },
   {
-    name:'张凯凯',
-    img:'./img/WechatIMG76.jpeg'
+    name: '张凯凯',
+    img: './img/WechatIMG76.jpeg'
   },
   {
-    name:'郭转生',
-    img:'./img/WechatIMG77.jpeg'
+    name: '郭转生',
+    img: './img/WechatIMG77.jpeg'
   },
   {
-    name:'安瑶',
-    img:'./img/WechatIMG78.jpeg'
+    name: '安瑶',
+    img: './img/WechatIMG78.jpeg'
   },
   {
-    name:'安宇鑫',
-    img:'./img/WechatIMG79.jpeg'
+    name: '安宇鑫',
+    img: './img/WechatIMG79.jpeg'
   },
   {
-    name:'白璐',
-    img:'./img/WechatIMG80.jpeg'
+    name: '白璐',
+    img: './img/WechatIMG80.jpeg'
   },
   {
-    name:'常洪强',
-    img:'./img/WechatIMG81.jpeg'
+    name: '常洪强',
+    img: './img/WechatIMG81.jpeg'
   },
   {
-    name:'陈元',
-    img:'./img/WechatIMG82.jpeg'
+    name: '陈元',
+    img: './img/WechatIMG82.jpeg'
   },
   {
-    name:'崔征云',
-    img:'./img/WechatIMG83.jpeg'
+    name: '崔征云',
+    img: './img/WechatIMG83.jpeg'
   },
   {
-    name:'宫照刚',
-    img:'./img/WechatIMG84.jpeg'
+    name: '宫照刚',
+    img: './img/WechatIMG84.jpeg'
   },
   {
-    name:'何喻',
-    img:'./img/WechatIMG85.jpeg'
+    name: '何喻',
+    img: './img/WechatIMG85.jpeg'
   },
   {
-    name:'焦帅',
-    img:'./img/WechatIMG86.jpeg'
+    name: '焦帅',
+    img: './img/WechatIMG86.jpeg'
   },
   {
-    name:'李方超',
-    img:'./img/WechatIMG87.jpeg'
+    name: '李方超',
+    img: './img/WechatIMG87.jpeg'
   },
   {
-    name:'李宏正',
-    img:'./img/WechatIMG88.jpeg'
+    name: '李宏正',
+    img: './img/WechatIMG88.jpeg'
   },
   {
-    name:'刘报童',
-    img:'./img/WechatIMG89.jpeg'
+    name: '刘报童',
+    img: './img/WechatIMG89.jpeg'
   },
   {
-    name:'刘力云',
-    img:'./img/WechatIMG90.jpeg'
+    name: '刘力云',
+    img: './img/WechatIMG90.jpeg'
   },
   {
-    name:'吕姣',
-    img:'./img/WechatIMG91.jpeg'
+    name: '吕姣',
+    img: './img/WechatIMG91.jpeg'
   },
   {
-    name:'秦振海',
-    img:'./img/WechatIMG92.jpeg'
+    name: '秦振海',
+    img: './img/WechatIMG92.jpeg'
   },
   {
-    name:'戚仁龙',
-    img:'./img/WechatIMG93.jpeg'
+    name: '戚仁龙',
+    img: './img/WechatIMG93.jpeg'
   },
   {
-    name:'宋慧超',
-    img:'./img/WechatIMG94.jpeg'
+    name: '宋慧超',
+    img: './img/WechatIMG94.jpeg'
   },
   {
-    name:'田利新',
-    img:'./img/WechatIMG95.jpeg'
+    name: '田利新',
+    img: './img/WechatIMG95.jpeg'
   },
   {
-    name:'王佳琦',
-    img:'./img/WechatIMG96.jpeg'
+    name: '王佳琦',
+    img: './img/WechatIMG96.jpeg'
   },
   {
-    name:'王凯杰',
-    img:'./img/WechatIMG97.jpeg'
+    name: '王凯杰',
+    img: './img/WechatIMG97.jpeg'
   },
   {
-    name:'王宁',
-    img:'./img/WechatIMG98.jpeg'
+    name: '王宁',
+    img: './img/WechatIMG98.jpeg'
   },
   {
-    name:'王莎莎',
-    img:'./img/WechatIMG99.jpeg'
+    name: '王莎莎',
+    img: './img/WechatIMG99.jpeg'
   },
   {
-    name:'韦宗伟',
-    img:'./img/WechatIMG100.jpeg'
+    name: '韦宗伟',
+    img: './img/WechatIMG100.jpeg'
   },
   {
-    name:'肖德超',
-    img:'./img/WechatIMG101.jpeg'
+    name: '肖德超',
+    img: './img/WechatIMG101.jpeg'
   },
   {
-    name:'徐泽锋',
-    img:'./img/WechatIMG103.jpeg'
+    name: '徐泽锋',
+    img: './img/WechatIMG103.jpeg'
   },
   {
-    name:'杨元',
-    img:'./img/WechatIMG104.jpeg'
+    name: '杨元',
+    img: './img/WechatIMG104.jpeg'
   },
   {
-    name:'闫兴茂',
-    img:'./img/WechatIMG105.jpeg'
+    name: '闫兴茂',
+    img: './img/WechatIMG105.jpeg'
   },
   {
-    name:'张建国',
-    img:'./img/WechatIMG106.jpeg'
+    name: '张建国',
+    img: './img/WechatIMG106.jpeg'
   },
   {
-    name:'张琦',
-    img:'./img/WechatIMG107.jpeg'
+    name: '张琦',
+    img: './img/WechatIMG107.jpeg'
   },
   {
-    name:'张文姗',
-    img:'./img/WechatIMG108.jpeg'
+    name: '张文姗',
+    img: './img/WechatIMG108.jpeg'
   },
   {
-    name:'赵宏斌',
-    img:'./img/WechatIMG109.jpeg'
+    name: '赵宏斌',
+    img: './img/WechatIMG109.jpeg'
   },
   {
-    name:'赵敏',
-    img:'./img/WechatIMG110.jpeg'
+    name: '赵敏',
+    img: './img/WechatIMG110.jpeg'
   }
 ]
